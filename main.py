e389e2a627dcd44f8bde32b53c8ed1c03d8f4c51
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-04-05 03:32:13 +0430

    2021.04.05 - 2

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-import asyncio
-import json
 import socket
+import socketserver
 import traceback
-import websockets
 
 
-async def connect(websocket, path):
-    data = await websocket.recv()
-    params = json.loads(data)
-    print(params)
-    try:
-        ret = "Hiyo Mahdi!"
-    except Exception as e:
-        ret = str(e.__class__)[8:-2] + ": " + str(e) + "\n" + ''.join(traceback.format_tb(e.__traceback__))
-    print("RESPONSE:", ret)
-    await websocket.send(ret)
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        try:
+            print("{} wrote:".format(self.client_address[0]), self.request.recv(1024).strip())
+            self.request.sendall(b"Fine")
+        except Exception as e:
+            print(str(e.__class__)[8:-2] + ": " + str(e) + "\n" + ''.join(traceback.format_tb(e.__traceback__)))
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-host, port = s.getsockname()[0], 3772
-print("RUNNING WEB-SOCKET SERVER AT " + host + ":" + str(port))
-start_server = websockets.serve(connect, host, port)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.connect(("8.8.8.8", 80))
+    HOST, PORT = s.getsockname()[0], 3772
+    s.close()
+print("RUNNING SOCKET SERVER AT " + HOST + ":" + str(PORT))
+server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+server.serve_forever()
