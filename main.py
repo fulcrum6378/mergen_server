8259a4e2e5fcb0e91a278e6cb4d241491786cad1
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-04-07 04:09:37 +0430

    2021.04.07 - 1

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import socket
 import socketserver
 import traceback
 
+last_client = None
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+
+class MyTCPHandler(socketserver.BaseRequestHandler):  # initiated in every request
+    def __init__(self, request, client_address, server):
+        super().__init__(request, client_address, server)
+
     def handle(self):
+        global last_client
         try:
-            print("{} wrote:".format(self.client_address[0]), self.request.recv(1024).strip())
-            self.request.sendall(b"Fine")
+            if self.client_address[0] != last_client:
+                if last_client is not None: print("")
+                print(str(self.client_address[0]) + ":")
+            last_client = self.client_address[0]
+            print(str(self.request.recv(1024))[2:-1])
+            # self.request.sendall(b"OK")
         except Exception as e:
             print(str(e.__class__)[8:-2] + ": " + str(e) + "\n" + ''.join(traceback.format_tb(e.__traceback__)))
 
@@ -18,4 +28,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.close()
 print("RUNNING SOCKET SERVER AT " + HOST + ":" + str(PORT))
 server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
-server.serve_forever()
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    pass  # THE END
+# FORCE KILL SERVER: kill $(sudo lsof -t -i:3772)
