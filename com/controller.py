a7a0a400d176933978680b3b8450aff65711dda8
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-05-26 16:49:53 +0430

    2021.05.26 - 1

--- com/controller.py
+++ com/controller.py
@@ -1,6 +1,7 @@
 import os
 import signal
 from socketserver import BaseRequestHandler
+from time import sleep
 from typing import Optional
 
 import moviepy.editor as mpy
@@ -12,14 +13,23 @@ from com.server import Server
 
 class Control(BaseRequestHandler):
     def handle(self):  # self.client_address[0]
-        print(str(self.request.recv(1024))[2:-1])
+        note = str(self.request.recv(1024))[2:-1]
+        if note == "start":
+            Controller.see()
+            Controller.hear()
+            sleep(1)
+            self.request.sendall(b"true")
+        elif note == "stop":
+            Controller.see(False)
+            Controller.hear(False)
 
 
 class Controller(Server):
+    vision: Optional[Server] = None
+    hearing: Optional[Server] = None
+
     def __init__(self):
         Server.__init__(self, 3772, Control)
-        self.vision: Optional[Server] = None
-        self.hearing: Optional[Server] = None
 
     def run(self) -> None:
         Server.run(self)
@@ -28,27 +38,31 @@ class Controller(Server):
         except KeyboardInterrupt:
             self.server.server_close()
 
-    def see(self, b=True) -> None:
+    @staticmethod
+    def see(b=True) -> None:
         if b:
-            self.vision = Server(3773, ImageHandler)
-            self.vision.start()
-        elif self.vision is not None:
-            self.vision.server.shutdown()
-            self.vision.kill()
-            self.vision = None
-            if self.hearing is None:
-                self.exit()
+            if Controller.vision is not None: return
+            Controller.vision = Server(3773, ImageHandler)
+            Controller.vision.start()
+        elif Controller.vision is not None:
+            Controller.vision.server.shutdown()
+            Controller.vision.kill()
+            Controller.vision = None
+            if Controller.hearing is None:
+                Controller.exit()
 
-    def hear(self, b=True) -> None:
+    @staticmethod
+    def hear(b=True) -> None:
         if b:
-            self.hearing = Server(3774, AudioHandler)
-            self.hearing.start()
-        elif self.hearing is not None:
-            self.hearing.server.shutdown()
-            self.hearing.kill()
-            self.hearing = None
-            if self.vision is None:
-                self.exit()
+            if Controller.hearing is not None: return
+            Controller.hearing = Server(3774, AudioHandler)
+            Controller.hearing.start()
+        elif Controller.hearing is not None:
+            Controller.hearing.server.shutdown()
+            Controller.hearing.kill()
+            Controller.hearing = None
+            if Controller.vision is None:
+                Controller.exit()
 
     @staticmethod
     def exit():
